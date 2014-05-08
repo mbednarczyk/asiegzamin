00c9605836e62585f3a2f82a04572991cbe9f6b5
Maciekek <maciekek@gmail.com>
2014-05-08 11:24:15 +0200

    testy widoku edit i show

--- spec/views/gists/edit.html.erb_spec.rb
+++ spec/views/gists/edit.html.erb_spec.rb
@@ -1,17 +1,38 @@
 require 'spec_helper'
-
+ 
 describe "gists/edit.html.erb" do
   before do
-    assign(:gist, stub_model(Gist, snippet: 'foo'))
+    assign(:gist, stub_model(Gist, snippet: "foo bar"))
   end
-
+ 
   it "display header" do
     render
     expect(rendered).to include("Edytowanie wrzutki")
   end
-
+ 
   it "renders _form partial" do
     render
     expect(view).to render_template(:partial => "_form", :count => 1)
   end
+ 
+  it "check existing language form" do
+    render
+    expect(rendered).to include("Język")
+  end
+ 
+  it "check existing language form" do
+    render
+    expect(rendered).to include("Opis")
+  end
+ 
+  it "displays Snippet data" do
+   
+  end
+ 
+  # it "displays description data" do
+  #   assign(:gist, stub_model(Gist, description: "some text"))
+  #   render
+  #   expect(rendered).to include("Description:")
+  #   expect(rendered).to include("some text")
+  # end
 end
--- spec/views/gists/show.html.erb_spec.rb
+++ spec/views/gists/show.html.erb_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
-
+ 
 describe "gists/show.html.erb" do
-  it "displays Lang data" do
-    assign(:gist, stub_model(Gist, lang: "ruby"))
-    render
-    expect(rendered).to include("Lang:")
-    expect(rendered).to include("ruby")
-  end
-
+  # it "displays Lang data" do
+  #   assign(:gist, stub_model(Gist, lang: "ruby"))
+  #   render
+  #   expect(rendered).to include("Język:")
+  #   expect(rendered).to include("ruby")
+  # end
+ 
   it "displays Snippet data" do
     assign(:gist, stub_model(Gist, snippet: "foo bar"))
     render
-    expect(rendered).to include("Snippet:")
+    expect(rendered).to include("Wrzutka:")
     expect(rendered).to match(/foo.*bar/)
   end
-
+ 
   it "displays description data" do
     assign(:gist, stub_model(Gist, description: "some text"))
     render
-    expect(rendered).to include("Description:")
+    expect(rendered).to include("Opis:")
     expect(rendered).to include("some text")
   end
 end
