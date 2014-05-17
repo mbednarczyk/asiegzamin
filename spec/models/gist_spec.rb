34390d84135f86224b8fe8b347dd5d47f798db95
Jan Kakol <jachu50012@gmail.com>
2014-05-17 10:25:39 +0200

    Przeniesienie testow gist do modelu gist

--- spec/models/gist_spec.rb
+++ spec/models/gist_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe Gist do
+	
+	describe "#new Gist-two params" do
+    it "takes two parameters and returns a Gist object" do
+      @gist = Gist.new  "", ""
+      @gist.should be_an_instance_of Gist
+    end
+end
+
+  describe "#new Gist-one params" do
+    it "takes one parameter and returns a Gist object" do
+      @gist = Gist.new  ""
+      @gist.should be_an_instance_of Gist
+    end
+end
+
+  describe "#new Gist-no params" do
+      it "takes no parameters and returns a Gist object" do
+        @gist = Gist.new  
+        @gist.should be_an_instance_of Gist
+      end
+  end
 end
