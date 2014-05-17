1ab30f34ca9afbca54cc193b59aa9dc56d23660a
Maciej Kucharski <maciekek@gmail.com>
2014-05-17 18:54:12 +0200

    poprawka testu widoku - show

--- spec/views/gists/show.html.erb_spec.rb
+++ spec/views/gists/show.html.erb_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
  
 describe "gists/show.html.erb" do
-  # it "displays Lang data" do
-  #   assign(:gist, stub_model(Gist, language: "ruby"))
-  #   render
-  #   # expect(rendered).to include("Język")
-  #   expect(rendered).to include("*")
-  # end
+  it "displays Lang data" do
+    assign(:gist, stub_model(Gist, lang: "ruby"))
+    render
+    expect(rendered).to include("ruby")
+  end
  
   it "displays Snippet data" do
     assign(:gist, stub_model(Gist, snippet: "foo bar"))
