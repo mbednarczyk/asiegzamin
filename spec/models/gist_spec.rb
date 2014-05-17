c89e3819b03083d5f8db53c6dc0241db30e77725
Jan Kakol <jachu50012@gmail.com>
2014-05-17 11:21:08 +0200

    Dodane testy do gits@model, sprawdzajace pola modelu

--- spec/models/gist_spec.rb
+++ spec/models/gist_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Gist do
-	
+
 	describe "#new Gist-two params" do
     it "takes two parameters and returns a Gist object" do
       @gist = Gist.new  "", ""
@@ -22,4 +22,26 @@ end
         @gist.should be_an_instance_of Gist
       end
   end
+
+  describe "CRUD tests" do
+    before :each do 
+      @gist = FactoryGirl.create(:gist) 
+  end 
+
+  describe "#snippet" do
+    it "returns the correct snippet" do
+        @gist.snippet.should eql "System.out.println(java)"
+    end
+end
+	describe "#lang" do
+    it "returns the correct lang" do
+        @gist.lang.should eql "java"
+    end
+end
+	describe "#description" do
+    it "returns the correct description" do
+        @gist.description.should eql "Sample of Java"
+    end
+end
+end
 end
