9882c1e987b18eec1df7a8f877445845e09298b9
Marek <mbednarczyk@live.com>
2014-05-18 13:17:21 +0200

    fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :github, '97ee065e9d33527648b3', '9b053190d91f07f050d5e3bf2af881855fdcc7d3', scope: "user:email"
-    provider :facebook, '1404092346537602', '6c0148be1344ed0ccb94357d75ab46dc'
+    provider :facebook, '228311714046195', 'c4de1d9a0ccf3a27182165fab6b2794c'
 
 end
