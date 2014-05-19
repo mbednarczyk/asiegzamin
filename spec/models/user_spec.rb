e2d9e7f5e10319b4e5eab4df7d795d674aecd07e
Marek <marekbmx0@op.pl>
2014-05-19 19:56:47 +0200

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,19 +9,19 @@ describe User do
       end
   end
 
-  describe "#new User-one parameter" do
-      it "takes one parameter and returns a User object" do
-        @user = User.new  ""
-        @user.should be_an_instance_of User
-      end
-  end
+  #describe "#new User-one parameter" do
+  #    it "takes one parameter and returns a User object" do
+  #      @user = User.new  ""
+  #      @user.should be_an_instance_of User
+  #    end
+  #end
 
-  describe "#new User-two params" do
-      it "takes two parameters and returns a User object" do
-        @user = User.new  "", ""
-        @user.should be_an_instance_of User
-      end
-  end
+  #describe "#new User-two params" do
+  #    it "takes two parameters and returns a User object" do
+  #      @user = User.new  "", ""
+  #      @user.should be_an_instance_of User
+  #    end
+  #end
 
   it "should allow login" do
 	    @user = FactoryGirl.create(:user)
