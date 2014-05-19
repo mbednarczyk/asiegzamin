2a308af0eb1b1c0a3a6d27df8ff89ec647da62f9
Marek <marekbmx0@op.pl>
2014-05-19 19:56:26 +0200

    Update gist_spec.rb

--- spec/models/gist_spec.rb
+++ spec/models/gist_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe Gist do
 
-	describe "#new Gist-two params" do
-    it "takes two parameters and returns a Gist object" do
-      @gist = Gist.new  "", ""
-      @gist.should be_an_instance_of Gist
-    end
-end
+# 	describe "#new Gist-two params" do
+#     it "takes two parameters and returns a Gist object" do
+#       @gist = Gist.new  "", ""
+#       @gist.should be_an_instance_of Gist
+#     end
+# end
 
-  describe "#new Gist-one params" do
-    it "takes one parameter and returns a Gist object" do
-      @gist = Gist.new  ""
-      @gist.should be_an_instance_of Gist
-    end
-end
+#   describe "#new Gist-one params" do
+#     it "takes one parameter and returns a Gist object" do
+#       @gist = Gist.new  ""
+#       @gist.should be_an_instance_of Gist
+#     end
+# end
 
   describe "#new Gist-no params" do
       it "takes no parameters and returns a Gist object" do
